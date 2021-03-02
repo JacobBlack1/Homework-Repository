c0f86d299c8b05ed6b2ede45d18ae0e7da4e37d3
JacobBlack1 <78884319+JacobBlack1@users.noreply.github.com>
2021-03-01 20:07:34 -0500

    java script

---
+++ Activity_12.js
@@ -0,0 +1,18 @@
+// Creates an array containing names of three students in the class  
+var students = ("Jacob", "Iris", "Sean")
+
+// Logs length of the students array
+console.log(names.length);
+
+// Now, write a console log introducing each student
+console.log("welcome to the class " + names[0] + ".");
+console.log("welcome to the class " + names[1] + ".");
+console.log("welcome to the class " + names[2] + ".");
+
+// Replace the first student in the array with a new student Zion
+student[0] = "Zion";
+
+// Use your Java =script to check if "Zion" is the first element in th array
+if (student(0) === "Zion") {
+    console.log(students[0] + " is in class!");
+}
